ce8271c0182904e267865b587421317675093241
wenzhang <zhangwen8277@gmail.com>
2021-03-24 23:48:33 +0100

    multiple modules project

--- service-x/src/test/java/com/demo/spring/cloud/config/yaml/BlackListPropertiesIntegrationTest.java
+++ service-x/src/test/java/com/demo/spring/cloud/config/yaml/BlackListPropertiesIntegrationTest.java
@@ -1,16 +1,11 @@
 package com.demo.spring.cloud.config.yaml;
 
 import com.demo.spring.cloud.config.properties.BlackListProperties;
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
-import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
