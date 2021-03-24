972a7e3612a4c40b4d9eb907ac00797b95d37694
wenzhang <zhangwen8277@gmail.com>
2021-03-24 22:46:42 +0100

    Make it as a gradle project.

---
+++ src/main/java/com/demo/spring/cloud/config/yaml/YamlConfigApplication.java
@@ -0,0 +1,12 @@
+package com.demo.spring.cloud.config.yaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamlConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YamlConfigApplication.class, args);
+    }
+}
